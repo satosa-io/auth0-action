5892bd388c97d41f1658d2d81b7f484b07c79679
unitehenry <unitehenry@gmail.com>
2023-06-28 15:46:52 -0400

    chore: dont build zod into bundle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { parseUser } from './auth0'
+
 export async function onExecutePostLogin (event: any, api: any): Promise<void> {
   console.log('SATOSA INTEGRATION: onExecutePostLogin')
+  const user = parseUser(event.user)
+  console.log(`USER ID: ${user.user_id}`)
 }
