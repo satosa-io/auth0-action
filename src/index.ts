53a57c778ca80f206b4de5ba938278d8ff0b7272
unitehenry <unitehenry@gmail.com>
2023-06-29 16:17:06 -0400

    feat: get interaction

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { parseUser } from './auth0'
-import { createConfiguration, getDocument } from './satosa'
+import { createConfiguration, getDocument, getInteraction } from './satosa'
 import { SATOSA_SERVICE_URL } from './constants'
 
 export async function onExecutePostLogin (event: any, api: any): Promise<void> {
@@ -9,9 +9,10 @@ export async function onExecutePostLogin (event: any, api: any): Promise<void> {
   const documentId = process.env.SATOSA_DOCUMENT_ID ?? ''
   const config = createConfiguration(serviceUrl, apiKey)
   const document = await getDocument(config, documentId)
+  const interaction = await getInteraction(config, document.id, document.published_revision, user.user_id)
   console.log(JSON.stringify(user))
   console.log(JSON.stringify(document))
-  // QUERY INTERACTIONS FOR USER ID AND DOCUMENT ID AND PUBLISHED REVISION
+  console.log(JSON.stringify(interaction))
   // IF NO INTERACTIONS
   // CREATE STATEFUL SESSION
   // REDIRECT TO HOSTED PAGE
--- src/satosa.ts
+++ src/satosa.ts
@@ -15,16 +15,53 @@ const Document = z.object({
   status: z.string()
 })
 
+const Interaction = z.object({
+  user_key: z.string(),
+  created_at: z.string().datetime(),
+  custom_attributes: z.record(z.string()),
+  document_id: z.string().ulid(),
+  id: z.string().ulid(),
+  modified_at: z.string().datetime(),
+  revision_id: z.number().int(),
+  system_attributes: z.object({
+    ip_address: z.string(),
+    user_agent: z.string()
+  }).partial(),
+  user_attributes: z.object({
+    email_address: z.string().email(),
+    family_name: z.string(),
+    given_name: z.string(),
+    phone_number: z.string()
+  }).partial()
+})
+
 export function createConfiguration (serviceUrl: string, apiKey: string): z.infer<typeof Configuration> {
   return Configuration.parse({ serviceUrl, apiKey })
 }
 
 export async function getDocument (config: z.infer<typeof Configuration>, documentId: string): Promise<z.infer<typeof Document>> {
   if (documentId === '') throw new Error('documentId is missing')
+  const url = new URL(config.serviceUrl + '/documents/' + documentId)
   const response = await axios({
     method: 'GET',
-    url: config.serviceUrl + '/documents/' + documentId,
+    url: url.toString(),
     headers: { Authorization: `Bearer ${config.apiKey}` }
   })
   return Document.parse(response.data)
+}
+
+export async function getInteraction (config: z.infer<typeof Configuration>, documentId: string, revisionId: number, userKey: string): Promise<z.infer<typeof Interaction> | undefined> {
+  if (documentId === '') throw new Error('documentId is missing')
+  if (userKey === '') throw new Error('userKey is missing')
+  const url = new URL(config.serviceUrl + '/interactions')
+  url.searchParams.set('document_id', documentId)
+  url.searchParams.set('revision_id', revisionId.toString())
+  url.searchParams.set('user_key', userKey)
+  const response = await axios({
+    method: 'GET',
+    url: url.toString(),
+    headers: { Authorization: `Bearer ${config.apiKey}` }
+  })
+  if (response.data?.items?.length > 0) { return Interaction.parse(response.data?.items?.pop()) }
+  return undefined
 }
