87c718f699d1eb27efd5892054fa0f6addd222a1
unitehenry <unitehenry@gmail.com>
2023-08-06 18:37:52 -0400

    feat: marketplace action secrets

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { SATOSA_HOSTED_URL, SATOSA_SERVICE_URL } from './constants'
 
 export async function onExecutePostLogin (event: any, api: any): Promise<void> {
   const user = parseUser(event.user)
-  const apiKey = event.secrets.API_KEY ?? ''
-  const hostedUrl = process.env.SATOSA_HOSTED_URL ?? SATOSA_HOSTED_URL
-  const serviceUrl = process.env.SATOSA_SERVICE_URL ?? SATOSA_SERVICE_URL
+  const apiKey = event.secrets.API_KEY || ''
+  const hostedUrl = process.env.SATOSA_HOSTED_URL || SATOSA_HOSTED_URL
+  const serviceUrl = process.env.SATOSA_SERVICE_URL || SATOSA_SERVICE_URL
   const customDomain = process.env.AUTH0_CUSTOM_DOMAIN
-  const callbackUrl = process.env.CALLBACK_URL ?? `https://${customDomain ?? process.env.AUTH0_DOMAIN ?? ''}/continue` ?? ''
-  const organizationId = process.env.SATOSA_ORGANIZATION_ID ?? ''
-  const documentId = process.env.SATOSA_DOCUMENT_ID ?? ''
+  const callbackUrl = process.env.CALLBACK_URL || `https://${customDomain ?? process.env.AUTH0_DOMAIN ?? ''}/continue` ?? ''
+  const organizationId = process.env.SATOSA_ORGANIZATION_ID || ''
+  const documentId = process.env.SATOSA_DOCUMENT_ID || ''
   const config = createConfiguration(serviceUrl, apiKey)
   const document = await getDocument(config, documentId)
   const interaction = await getInteraction(config, document.id, document.published_revision, user.user_id)
