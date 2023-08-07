24310fae4914a32ff6d5224d059c811e6e50f58f
unitehenry <unitehenry@gmail.com>
2023-08-06 20:02:32 -0400

    test: action should export functions

--- test/action.test.ts
+++ test/action.test.ts
@@ -1,10 +1,18 @@
-import { onContinuePostLogin } from '../src/index'
+import { onContinuePostLogin, onExecutePostLogin } from '../src/index'
 
-test('onContinuePostLogin to be undefined', async () => {
+test('action to export onContinuePostLogin function', async () => {
+  expect(typeof onContinuePostLogin).toBe('function')
+})
+
+test('action to export onExecutePostLogin function', async () => {
+  expect(typeof onExecutePostLogin).toBe('function')
+})
+
+test('onContinuePostLogin result to be undefined', async () => {
   // eslint-disable-next-line
   expect(await onContinuePostLogin(undefined, undefined)).toBe(undefined)
 })
 
-// test('onExecutePostLogin to redirect to hosted document page with session', async () => {
-//
-// })
+test('onExecutePostLogin to redirect to hosted document page with session', async () => {
+  expect(typeof onExecutePostLogin).toBe('function')
+})
