3b7ebe35df3578c8c9482385223c1f1ba94d2338
unitehenry <unitehenry@gmail.com>
2023-08-06 21:21:23 -0400

    refactor: original rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,8 +2,6 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 
-type ReplaceConfigValues = Record<string, string>
-
 const IGNORE_BUNDLE_MODULES = ['axios', 'zod']
 
 const nodeResolveOptions = {
@@ -13,27 +11,17 @@ const nodeResolveOptions = {
   }
 }
 
-const variables = [
-  'AUTH0_CUSTOM_DOMAIN',
-  'AUTH0_DOMAIN',
-  'CALLBACK_URL',
-  'SATOSA_ORGANIZATION_ID',
-  'SATOSA_SERVICE_URL',
-  'SATOSA_DOCUMENT_ID',
-  'SATOSA_HOSTED_URL'
-]
-
 const replaceConfig = {
-  preventAssignment: true,
-  values: variables.reduce((values: ReplaceConfigValues, variable: string) => {
-    if (process.env.MARKETPLACE_DEPLOY === 'true') {
-      values[`process.env.${variable}`] = `event.secrets.${variable}`
-      return values
-    }
-    const eValue = process.env[variable]
-    values[`process.env.${variable}`] = eValue ? `"${eValue}"` : 'undefined'
-    return values
-  }, {})
+  values: {
+    'process.env.AUTH0_CUSTOM_DOMAIN': process.env.AUTH0_CUSTOM_DOMAIN ? `"${process.env.AUTH0_CUSTOM_DOMAIN}"` : 'undefined',
+    'process.env.AUTH0_DOMAIN': process.env.AUTH0_DOMAIN ? `"${process.env.AUTH0_DOMAIN}"` : 'undefined',
+    'process.env.CALLBACK_URL': process.env.CALLBACK_URL ? `"${process.env.CALLBACK_URL}"` : 'undefined',
+    'process.env.SATOSA_ORGANIZATION_ID': process.env.SATOSA_ORGANIZATION_ID ? `"${process.env.SATOSA_ORGANIZATION_ID}"` : 'undefined',
+    'process.env.SATOSA_SERVICE_URL': process.env.SATOSA_SERVICE_URL ? `"${process.env.SATOSA_SERVICE_URL}"` : 'undefined',
+    'process.env.SATOSA_DOCUMENT_ID': process.env.SATOSA_DOCUMENT_ID ? `"${process.env.SATOSA_DOCUMENT_ID}"` : 'undefined',
+    'process.env.SATOSA_HOSTED_URL': process.env.SATOSA_HOSTED_URL ? `"${process.env.SATOSA_HOSTED_URL}"` : 'undefined'
+  },
+  preventAssignment: true
 }
 
 export default {
