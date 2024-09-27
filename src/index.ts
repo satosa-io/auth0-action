93a2f6e52d7724104f9e01038360ae7173a1d57a
unitehenry <unitehenry@gmail.com>
2024-09-27 15:12:51 -0700

    fix: manual deploy vars

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import { SATOSA_HOSTED_URL, SATOSA_SERVICE_URL } from './constants'
 export async function onExecutePostLogin (event: any, api: any): Promise<void> {
   const user = parseUser(event.user)
   const apiKey = event.secrets.API_KEY ?? ''
-  const hostedUrl = process.env?.SATOSA_HOSTED_URL || event.secrets?.SATOSA_HOSTED_URL || event.configuration?.SATOSA_HOSTED_URL || SATOSA_HOSTED_URL
-  const serviceUrl = process.env?.SATOSA_SERVICE_URL || event.secrets?.SATOSA_SERVICE_URL || event.configuration?.SATOSA_SERVICE_URL || SATOSA_SERVICE_URL
-  const customDomain = process.env?.AUTH0_CUSTOM_DOMAIN || event.secrets?.AUTH0_CUSTOM_DOMAIN || event.configuration?.AUTH0_CUSTOM_DOMAIN || ''
-  const authDomain = customDomain || process.env?.AUTH0_DOMAIN || event.secrets?.AUTH0_DOMAIN || event.configuration?.AUTH0_DOMAIN || ''
+  const hostedUrl = process.env.SATOSA_HOSTED_URL || event.secrets?.SATOSA_HOSTED_URL || event.configuration?.SATOSA_HOSTED_URL || SATOSA_HOSTED_URL
+  const serviceUrl = process.env.SATOSA_SERVICE_URL || event.secrets?.SATOSA_SERVICE_URL || event.configuration?.SATOSA_SERVICE_URL || SATOSA_SERVICE_URL
+  const customDomain = process.env.AUTH0_CUSTOM_DOMAIN || event.secrets?.AUTH0_CUSTOM_DOMAIN || event.configuration?.AUTH0_CUSTOM_DOMAIN || ''
+  const authDomain = customDomain || process.env.AUTH0_DOMAIN || event.secrets?.AUTH0_DOMAIN || event.configuration?.AUTH0_DOMAIN || ''
   const continueUrl = `https://${authDomain as string}/continue`
-  const callbackUrl = process.env?.CALLBACK_URL || event.secrets?.CALLBACK_URL || event.configuration?.CALLBACK_URL || continueUrl
-  const organizationId = process.env?.SATOSA_ORGANIZATION_ID || event.secrets?.SATOSA_ORGANIZATION_ID || event.configuration?.SATOSA_ORGANIZAION_ID || ''
-  const documentId = process.env?.SATOSA_DOCUMENT_ID || event.secrets?.SATOSA_DOCUMENT_ID || event.configuration?.SATOSA_DOCUMENT_ID || ''
+  const callbackUrl = process.env.CALLBACK_URL || event.secrets?.CALLBACK_URL || event.configuration?.CALLBACK_URL || continueUrl
+  const organizationId = process.env.SATOSA_ORGANIZATION_ID || event.secrets?.SATOSA_ORGANIZATION_ID || event.configuration?.SATOSA_ORGANIZAION_ID || ''
+  const documentId = process.env.SATOSA_DOCUMENT_ID || event.secrets?.SATOSA_DOCUMENT_ID || event.configuration?.SATOSA_DOCUMENT_ID || ''
   const config = createConfiguration(serviceUrl, apiKey)
   const document = await getDocument(config, documentId)
   const interaction = await getInteraction(config, document.id, document.published_revision, user.user_id)
